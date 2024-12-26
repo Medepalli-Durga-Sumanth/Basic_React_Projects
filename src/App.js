d5cac3dd7fed9b477fb1931e1e526b839632ea0d
Medepalli-Durga-Sumanth <medepallisumanth29@gmail.com>
2024-12-26 20:01:24 +0530

    props concept

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Home from './Home'
+import Contact from './Contact'
+import About from './About'
+import Greeting from './Greeting';
+function App() {
+  return (
+    <div>
+    <h1>Hello from Reactj</h1>
+    <Greeting name = {'Akash'}/>
+    <Greeting name = {'Ayush'} />
+    {/* <Home name = {'Pooja'} age = {24} />
+    <About />
+    <Contact /> */}
+    </div>
+  );
+}
+export default App;
